233cc4315deb2610fdd64da000316f3977f4e7bc
dreamey-eth <57111814+dreamey-eth@users.noreply.github.com>
2023-12-11 19:37:28 +0100

    feat: wallet connect integration

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,39 @@ import Market from "./pages/Market.jsx";
 import Docs from "./pages/Docs.jsx";
 import Portfolio from "./pages/Portfolio.jsx";
 
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+  mainnet,
+  arbitrum,
+} from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient } = configureChains(
+  [arbitrum],
+  [
+    alchemyProvider({ apiKey: '2s-1F2BiWb0o7mA2LieeKX4j46A0YhM3' }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Lila Finance',
+  projectId: 'c0027ae711b4888e46a4a700b7274a56',
+  chains
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -28,6 +61,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains} modalSize="compact">
+        <RouterProvider router={router} />
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>
 );
